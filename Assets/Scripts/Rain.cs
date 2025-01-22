c2af5fa3e2f9aba13f56d795d21fd372c1ac0992
000SIN <sin.dev200@gmail.com>
2025-01-22 23:59:38 +0900

    .
    .

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -1,3 +1,7 @@
+using UnityEngine;
+
+[Rain.cs]
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,44 +32,44 @@ public class NewBehaviourScript : MonoBehaviour
         {
             size = 0.8f;
             score = 1;
-            renderer.color = new Color(100/255f, 100/255f, 1f, 1f);
+            renderer.color = new Color(100 / 255f, 100 / 255f, 1f, 1f);
         }
         else if (type == 2)
         {
             size = 1.0f;
             score = 2;
-            renderer.color = new Color(130/255f, 130/255f, 1f, 1f);
+            renderer.color = new Color(130 / 255f, 130 / 255f, 1f, 1f);
         }
         else if (type == 3)
         {
             size = 1.2f;
             score = 3;
+            renderer.color = new Color(150 / 255f, 150 / 255f, 1f, 1f);
         }
-        renderer.color = new Color(150/255f, 150/255f, 1f, 1f);
 
-        transform.localScale = new Vector3 (size, size, 0);
+        transform.localScale = new Vector3(size, size, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-        //충돌이 일어나는 순간
+    //충돌이 일어나는 순간
     {
-        if(collision.gameObject.CompareTag("Ground"))
-            //만약 Tag가 Ground인 게임 오브젝트와 충돌하였을 때
+        if (collision.gameObject.CompareTag("Ground"))
+        //만약 Tag가 Ground인 게임 오브젝트와 충돌하였을 때
         {
             Destroy(this.gameObject);
             //this = Rain 스크립트 자기 자신. 스스로를 파괴하여라.
         }
 
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             GameManager.Instance.AddScore(score);
-            Destroy(this.gameObject) ;
+            Destroy(this.gameObject);
         }
     }
 }
